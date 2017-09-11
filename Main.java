67df6286f098cd724b7577bcb11d2748d91fb87e
divya5a9 <31822083+divya5a9@users.noreply.github.com>
2017-09-11 13:51:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+ public class Main {
+
+	public static void main(String[] args) {
+	    StudentGroup b=new studentGroup[1];
+	    Student s[]=new Student (id,fullName,birthDate,avgMark);
+	   b.s.setStudent( 102,divya,1996-02-17,45);
+
+
+
+	}
+
+}
